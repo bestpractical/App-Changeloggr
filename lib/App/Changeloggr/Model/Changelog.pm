b2372207d48d43a2eed4421b5da1fef5f051ed4c
Shawn M Moore <sartak@gmail.com>
2009-02-26 19:59:10 -0500

    Changelog->current_user_can

--- lib/App/Changeloggr/Model/Changelog.pm
+++ lib/App/Changeloggr/Model/Changelog.pm
@@ -14,5 +14,18 @@ use App::Changeloggr::Record schema {
         default is 0;
 };
 
+sub current_user_can {
+    my $self  = shift;
+    my $right = shift;
+
+    return 1 if $self->current_user->is_superuser;
+
+    # anyone can create and read changelogs
+    return 1 if $right eq 'create' || $right eq 'read';
+
+    # but not delete or update. those must happen as root
+    return 0;
+}
+
 1;
 
