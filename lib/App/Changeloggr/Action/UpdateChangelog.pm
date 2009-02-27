980c1b9acfe0a50eafba66636b87a34ac0d05df4
Shawn M Moore <sartak@gmail.com>
2009-02-26 22:05:01 -0500

    Add an UpdateChangelog action which confirms that you have the admin token before letting you update

---
+++ lib/App/Changeloggr/Action/UpdateChangelog.pm
@@ -0,0 +1,29 @@
+package App::Changeloggr::Action::UpdateChangelog;
+use strict;
+use warnings;
+use base 'Jifty::Action::Record::Create';
+
+use JiftyX::ModelHelpers;
+
+sub record_class { 'App::Changeloggr::Model::Changelog' }
+
+sub validate_admin_token {
+    my $self        = shift;
+    my $admin_token = shift;
+
+    if ($self->record->as_superuser->admin_token eq $admin_token) {
+        return $self->validation_ok('admin_token');
+    }
+    else {
+        return $self->validation_error(admin_token => "You do not have permission to update this changelog.");
+    }
+}
+
+sub take_action {
+    my $self = shift;
+    $self->record->current_user(App::Changeloggr::CurrentUser->superuser);
+    $self->SUPER::take_action(@_);
+}
+
+1;
+
