cbbcb5fe0ecd31d1d7d87a0c52eb2adbde263324
Shawn M Moore <sartak@gmail.com>
2009-02-26 19:11:41 -0500

    Add Changelog and Change model classes

---
+++ lib/App/Changeloggr/Model/Changelog.pm
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+package App::Changeloggr::Model::Changelog;
+use Jifty::DBI::Schema;
+
+use App::Changeloggr::Record schema {
+
+};
+
+# Your model-specific methods go here.
+
+1;
+
