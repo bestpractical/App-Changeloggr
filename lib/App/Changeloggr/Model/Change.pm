3674a77223d7f8e276105372c566e119abf5e6f4
Shawn M Moore <sartak@gmail.com>
2009-02-26 19:30:23 -0500

    Changes have a Changelog

--- lib/App/Changeloggr/Model/Change.pm
+++ lib/App/Changeloggr/Model/Change.pm
@@ -5,6 +5,9 @@ package App::Changeloggr::Model::Change;
 use Jifty::DBI::Schema;
 
 use App::Changeloggr::Record schema {
+  column changelog =>
+        refers_to App::Changeloggr::Model::Changelog;
+
   column author =>
         type is 'text',
         label is 'Author';
