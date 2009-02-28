7b9fa010ed6591aedc440f3b2f85f8c5694781a3
Shawn M Moore <sartak@gmail.com>
2009-02-27 21:07:44 -0500

    Begin an AddChanges action

---
+++ lib/App/Changeloggr/Action/AddChanges.pm
@@ -0,0 +1,49 @@
+package App::Changeloggr::Action::AddChanges;
+use strict;
+use warnings;
+
+use JiftyX::ModelHelpers;
+
+use Jifty::Param::Schema;
+use Jifty::Action schema {
+    param admin_token =>
+        type is 'text';
+
+    param changes =>
+        type is 'text',
+        render as 'textarea',
+        is mandatory,
+        hints is 'We accept the output of svn log and git log.';
+};
+
+sub get_changelog {
+    my $self = shift;
+
+    my $changelog = Changelog(
+        admin_token => $self->argument_value('admin_token'),
+        { current_user => App::Changeloggr::CurrentUser->superuser },
+    );
+
+    return $changelog;
+}
+
+sub validate_admin_token {
+    my $self        = shift;
+    my $admin_token = shift;
+
+    my $changelog = $self->get_changelog;
+    if ($changelog->id) {
+        return $self->validation_ok('admin_token');
+    }
+    else {
+        return $self->validation_error(admin_token => "You do not have permission to add changes to this changelog.");
+    }
+}
+
+sub take_action {
+    my $self = shift;
+    my $changelog = $self->get_changelog;
+}
+
+1;
+
