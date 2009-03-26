513a9de98fd58a54f3a926afea5ff5b5ed3be77d
Shawn M Moore <sartak@gmail.com>
2009-03-26 17:58:07 -0400

    Add a ChangesCollection which has a parse-and-create loop

---
+++ lib/App/Changeloggr/Model/ChangesCollection.pm
@@ -0,0 +1,34 @@
+package App::Changeloggr::Model::ChangesCollection;
+use strict;
+use warnings;
+use base 'App::Changeloggr::Collection';
+use Params::Validate 'SCALAR';
+
+sub create_from_text {
+    my $self = shift;
+    my %args = validate(@_, {
+        text      => { type => SCALAR },
+        changelog => { isa => 'App::Changeloggr::Model::Changelog' },
+    });
+
+    my $text      = $args{text};
+    my $changelog = $args{changelog};
+    my $count     = 0;
+
+    while (length $text) {
+        my $change = App::Changeloggr::Model::Change->new;
+        my $newtext = $change->create_from_text(
+            changelog => $changelog,
+            text      => $text,
+        );
+
+        last if !defined($newtext);
+        $text = $newtext;
+        ++$count;
+    }
+
+    return ($count, $text;
+}
+
+1;
+
