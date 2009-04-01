40cd8be887a26fbc86d381cead7d5bbed5f0402b
Shawn M Moore <sartak@gmail.com>
2009-04-01 14:09:02 -0400

    Similar superuser-ify action for DeleteTag

---
+++ lib/App/Changeloggr/Action/DeleteTag.pm
@@ -0,0 +1,13 @@
+package App::Changeloggr::Action::DeleteTag;
+use strict;
+use warnings;
+use base 'App::Changeloggr::Action::Mixin::RequiresAdminToken', 'Jifty::Action::Record::Delete';
+
+sub take_action {
+    my $self = shift;
+    $self->record->current_user(App::Changeloggr::CurrentUser->superuser);
+    $self->SUPER::take_action(@_);
+}
+
+1;
+
