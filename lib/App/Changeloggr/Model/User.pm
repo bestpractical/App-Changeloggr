2781f542f06fe5ea1b1f7f16c077043d59f7a1fe
Shawn M Moore <sartak@gmail.com>
2009-04-29 16:23:20 -0400

    current_user_can for users

--- lib/App/Changeloggr/Model/User.pm
+++ lib/App/Changeloggr/Model/User.pm
@@ -17,5 +17,24 @@ use App::Changeloggr::Record schema {
 
 sub since { '0.0.4' }
 
+sub current_user_can {
+    my $self  = shift;
+    my $right = shift;
+    my %args  = @_;
+
+    # the current user can do anything to his account
+    my $session_id = $self->__value('session_id');
+    return 1 if $session_id eq Jifty->web->session->id;
+
+    # users are private except name
+    return 1 if $right eq 'read' && $args{column} ne 'name';
+
+    # anyone can create accounts
+    return 1 if $right eq 'create';
+
+    # but otherwise users are locked down
+    return $self->SUPER::current_user_can($right, %args);
+}
+
 1;
 
