c519281290c9c0a827c392aaff9bc8b9fb9f1a7f
Shawn M Moore <sartak@gmail.com>
2009-04-29 17:54:14 -0400

    Avoid undef warning

--- lib/App/Changeloggr/Model/User.pm
+++ lib/App/Changeloggr/Model/User.pm
@@ -24,7 +24,7 @@ sub current_user_can {
 
     # the current user can do anything to his account
     my $session_id = $self->__value('session_id');
-    return 1 if $session_id eq Jifty->web->session->id;
+    return 1 if $session_id eq (Jifty->web->session->id||'');
 
     # users are private except name
     return 1 if $right eq 'read' && $args{column} ne 'name';
