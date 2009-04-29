e4dfb826438154bd342f9ad60f7649cb96ceb5e0
Shawn M Moore <sartak@gmail.com>
2009-04-29 18:21:56 -0400

    force usernames to be distinct

--- lib/App/Changeloggr/Model/User.pm
+++ lib/App/Changeloggr/Model/User.pm
@@ -7,7 +7,8 @@ use Jifty::DBI::Schema;
 use App::Changeloggr::Record schema {
     column name =>
         type is 'text',
-        label is 'Name';
+        label is 'Name',
+        is distinct;
 
     column session_id =>
         type is 'text',
