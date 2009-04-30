5b0879f20978197bbb1b2e1bc44155c2f0903319
Shawn M Moore <sartak@gmail.com>
2009-04-30 16:48:39 -0400

    CreateVote can create tags

--- lib/App/Changeloggr/Action/CreateVote.pm
+++ lib/App/Changeloggr/Action/CreateVote.pm
@@ -8,12 +8,29 @@ sub validate_tag {
     my $self = shift;
     my $tag  = shift;
 
+    my $changelog = Changelog($self->argument_value('changelog_id'));
+    if (!$changelog->id) {
+        my $change = Change($self->argument_value('change_id'));
+        $changelog = $change->changelog;
+    }
+
+    return $self->validation_error(change_id => "No change provided")
+        if !$changelog->id;
+
+    # if the admin wants incremental tag creation for this project, then
+    # add this tag before voting
+    if (!$changelog->has_tag($tag)) {
+        my ($ok, $msg) = $changelog->add_tag($tag);
+        return $self->validation_ok('tag') if $ok;
+        return $self->validation_error(tag => $msg);
+    }
+
     # if the admin has set up a set of tags for this project, then validate
     # against that list
-    my $valid_tags = Changelog($self->argument_value('changelog'))->tags;
+    my $valid_tags = $changelog->tags;
     if ($valid_tags->count) {
         while (my $valid_tag = $valid_tags->next) {
-            return $self->validation_ok('tag') if $valid_tag eq $tag;
+            return $self->validation_ok('tag') if $valid_tag->text eq $tag;
         }
 
         return $self->validation_error(tag => "That is not a valid tag for this changelog.");
