c8e004d3acaa6a2632758a1a7e57076ef6d25659
Shawn M Moore <sartak@gmail.com>
2009-05-12 00:46:23 -0400

    Order changes in a category by their importance.
        We probably want to just filter out any changes that hit some
        low-importance threshold.

--- lib/App/Changeloggr/OutputFormat/Perl.pm
+++ lib/App/Changeloggr/OutputFormat/Perl.pm
@@ -25,7 +25,13 @@ EOT
 
     for my $cat (sort keys %{$args{categories}}) {
         $str .= "\n=head1 $cat\n\n";
-        for my $change (@{$args{categories}{$cat}}) {
+
+        my @changes = map  { $_->[0] }
+                      sort { $a->[1] <=> $b->[1] }
+                      map  { [$_, $_->numeric_importance] }
+                      @{$args{categories}{$cat}}
+
+        for my $change (@changes) {
             my($summary) = $change->message =~ /\A(.*)$/m;
             $str .= "=head2 $summary\n\n" . $change->message . "\n";
         }
