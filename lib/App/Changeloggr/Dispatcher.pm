a17d94d6d5e89161aea8c81bd2b64b9a31d10d9a
Shawn M Moore <sartak@gmail.com>
2009-05-22 14:17:27 -0400

    Remove another debug warning

--- lib/App/Changeloggr/Dispatcher.pm
+++ lib/App/Changeloggr/Dispatcher.pm
@@ -24,7 +24,6 @@ on '/admin/created-changelog' => run {
 };
 
 before '/changelog/*' => run {
-    warn "Checking before $1";
     my $cl = Changelog( name => $1 );
     return unless $cl->id and $cl->current_user_is_admin;
     Jifty->web->navigation->child(
