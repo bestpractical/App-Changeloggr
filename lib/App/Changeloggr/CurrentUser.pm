9671c372661bd58d6ecdb0476d0cd4a75cb144b2
Shawn M Moore <sartak@gmail.com>
2009-07-29 14:11:03 -0400

    get and set for the user's starting position for a changelog

--- lib/App/Changeloggr/CurrentUser.pm
+++ lib/App/Changeloggr/CurrentUser.pm
@@ -12,5 +12,34 @@ sub is_staff {
     return $user->access_level eq 'staff';
 }
 
+sub position_for {
+    my $self      = shift;
+    my $changelog = shift;
+
+    my $session = Jifty::Web::Session->new;
+    $session->load;
+
+    my $key = "changelog-" . $changelog->id;
+    my $position = $session->get($key);
+    if (!defined($position)) {
+        $position = $changelog->get_starting_position;
+        $self->set_position_for($changelog, $position);
+    }
+
+    return $position;
+}
+
+sub set_position_for {
+    my $self      = shift;
+    my $changelog = shift;
+    my $position  = shift;
+
+    my $session = Jifty::Web::Session->new;
+    $session->load;
+
+    my $key = "changelog-" . $changelog->id;
+    $session->set($key => $position);
+}
+
 1;
 
